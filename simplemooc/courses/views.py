2df043b90f423e982af2a28ed8fc038f80acba0f
hugogomess <hugogomes02@gmail.com>
2019-05-26 18:46:14 -0300

    add courses template

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Course
 
 def index(request):
-    return render(request, 'index.html')
+    courses = Course.objects.find()
+    context = {'courses': courses}
+
+    return render(request, 'courses/index.html', context)
