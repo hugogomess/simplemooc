d31157f9eb031583c1ccfec077cff2b4d7318fe9
hugogomess <hugogomes02@gmail.com>
2019-05-26 23:48:28 -0300

    add course contact and send emails

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
     courses = Course.objects.find()
@@ -9,6 +10,19 @@ def index(request):
 
 def course(request, slug):
     course = get_object_or_404(Course, slug = slug)
-    context = {'course': course}
+    context = {}
+
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_email(course)
+            form = ContactCourse()
+
+    else:
+        form = ContactCourse()
+
+    context['course'] = course
+    context['form'] = form
 
     return render(request, 'courses/course.html', context)
